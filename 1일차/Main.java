fa6a2f9f1d7b3e49119314079ad372cfc0214714
이근호 <dbalee119@naver.com>
2026-04-08 15:13:19 +0900

    출력문 주석 연습 완료

--- 1일차/Main.java
+++ 1일차/Main.java
@@ -1,4 +1,7 @@
 public static void main(String[] args) {
-    System.out.println("hello world");
+    System.out.println("hello world"); // 출력문 주석
+
+    /* 출력문 주석2 */
     System.out.println("hi");
+
 }
